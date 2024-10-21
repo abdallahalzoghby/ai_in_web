4055951a55a8c59e53e01ec3db2c934781ef6c1c
abdallahalzoghby <142046244+abdallahalzoghby@users.noreply.github.com>
2024-10-22 00:39:57 +0300

    my_first_project

--- app/main.py
+++ app/main.py
@@ -1,14 +1,6 @@
 from fastapi import FastAPI
-#import tensorflow as tf
-
-
-
 app = FastAPI()
-
-#model = tf.keras.models.load_model('C:\Users\abdal\Downloads\model\app_test\my_ml_project\app\random_forest_model.h5')
-#print("Model loaded successfully!")
-
 
 @app.get("/hello")
 def read_hello():
-    return {"message": "Hello World! I am here"}
+    return {"message": "Hello World! I m abdallah alzoghby is update  and now  and zamelak"}
